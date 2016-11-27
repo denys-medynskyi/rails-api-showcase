8db0aaf243b8c51b4ffc93980b42a18b2c1b923b
Denys Medynskyi <denys.medynskyi@gmail.com>
2016-11-27 21:54:54 +0200

    caching

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,3 +2,4 @@ class Post < ApplicationRecord
   belongs_to :user, optional: true
   validates :title, :body, presence: true
 end
+
--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,4 +1,5 @@
 class PostSerializer < ActiveModel::Serializer
+  # cache key: 'post', expires_in: 1.hour
   attributes :id, :title, :body
   belongs_to :user
 end
